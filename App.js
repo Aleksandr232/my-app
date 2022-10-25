68e52ddc6a01df148b45051b198c434e2ab25067
Aleksandr232 <aleksrowing232@gmail.com>
2022-10-25 17:16:04 +0300

    app dark switch

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   const {colorScheme, toggleColorScheme} = useColorScheme();
   return (
     <View className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
-      <Switch value={colorScheme === 'dark'}/>
+      <Switch value={colorScheme === 'dark'} onChange={toggleColorScheme}/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
